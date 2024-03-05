510dbe2b64d2ea86a1c917bc2e84a8b08a08b1e9
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-03-05 14:13:06 -0300

    feat: adding prettier formatting rules #2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,13 @@ import { createApplication, createConfig } from "./app.js";
 
 async function main() {
   try {
-    const config = createConfig(3000, 'development');
+    const config = createConfig(3000, "development");
     const application = await createApplication(config);
-    application.listenAndServe()
+    application.listenAndServe();
   } catch (error) {
-    console.error('Error starting application:', error);
-    process.exit(1); 
+    console.error("Error starting application:", error);
+    process.exit(1);
   }
 }
 
 main();
-
