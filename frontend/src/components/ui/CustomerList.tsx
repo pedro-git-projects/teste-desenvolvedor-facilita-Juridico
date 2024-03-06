12ff3556092029aadc6a335453616d09bf0e7ab2
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-03-05 21:54:47 -0300

    feat: filtering customers on frontend #19

--- frontend/src/components/ui/CustomerList.tsx
+++ frontend/src/components/ui/CustomerList.tsx
@@ -1,39 +1,51 @@
 import { useEffect, useState } from "react";
 import CustomerResponse from "../../interfaces/CustomerResponse";
 import CustomerCard from "./CustomerCard";
+import SelectFilter from "./SelectFilter";
 
 const CustomerList: React.FC = () => {
   const [customers, setCustomers] = useState<CustomerResponse[]>([]);
+  const [filteredCustomers, setFilteredCustomers] = useState<
+    CustomerResponse[]
+  >([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/list-customers");
+      const data = await response.json();
+      setCustomers(data);
+      setFilteredCustomers(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/list-customers");
-        const data = await response.json();
-        setCustomers(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
     fetchData();
   }, []);
 
+  const applyFilters = (filteredData: CustomerResponse[]) => {
+    setFilteredCustomers(filteredData);
+  };
+
   return (
-    <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
-      {customers.map(customer => (
-        <CustomerCard
-          key={customer.id}
-          id={customer.id}
-          created_at={customer.created_at}
-          nome={customer.nome}
-          email={customer.email}
-          telefone={customer.telefone}
-          coordenada_x={customer.coordenada_x}
-          coordenada_y={customer.coordenada_y}
-        />
-      ))}
-    </div>
+    <>
+      <SelectFilter applyFilters={applyFilters} />
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
+        {filteredCustomers.map(customer => (
+          <CustomerCard
+            key={customer.id}
+            id={customer.id}
+            created_at={customer.created_at}
+            nome={customer.nome}
+            email={customer.email}
+            telefone={customer.telefone}
+            coordenada_x={customer.coordenada_x}
+            coordenada_y={customer.coordenada_y}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
